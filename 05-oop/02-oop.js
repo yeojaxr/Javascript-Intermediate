555947996eda7176b87717043a5f55972a363a29
yeojaxr <ikenurulfatihah23@gmail.com>
2021-10-11 20:45:16 +0700

    oop-new 2

---
+++ 05-oop/02-oop.js
@@ -0,0 +1,19 @@
+// 1. Apa itu Encapsulation? Mengapa kita membutuhkannya?
+// Jawab:
+// Encapsulation adalah cara untuk membatasi akses langsung ke properti atau method dari sebuah objek.
+// Encapsulation dibutuhkan saat kita ingin suatu data/variabel yang ada di dalam suatu kelas tidak bisa diubah/re-assign lagi.
+
+// 2. Apa itu Abstraction? Mengapa kita membutuhkannya?
+// Jawab:
+// Abstraction adalah sebuah teknik untuk menyembunyikan detail tertentu dari sebuah objek/method dan hanya menampilkan fungsionalitas atau fitur penting dari objek tersebut.
+// Abstraction dibutuhkan saat kita ingin menyembunyika suatu detail dari objek dan akan hanya menampilkan fungsionalitasnya saja
+
+// 3. Apa itu Inheritance? Mengapa kita membutuhkannya?
+// Jawab:
+// Inheritance dalam OOP adalah sebuah proses dimana sebuah class mewariskan property dan methodnya ke class lain atau childnya.
+// inheritance dibutuhkan ketika kita ingin mengambil objek dan method yang ada di class parent dan tidak perlu membuat objek dan method yang sama di class child nya
+
+// 4. Apa itu Polymorpishm? Mengapa kita membutuhkannya?
+// Jawab:
+// Polymorpishm adalah kemampuan untuk membuat variabel, fungsi, atau objek yang memiliki banyak bentuk.
+// Polymorpishm sama sama seperti inheritance, hanya saja di polymorpishm metode nya bisa diubah dan tidak perlu mengikuti method yang ada pada class parent
